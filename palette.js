542df888f4ebf2ded28d954feaec24aab5aa0daa
nnah <nahnope@web.de>
2015-07-13 16:43:01 +0200

    helldunkelfarbveränderung
    h

--- palette.js
+++ palette.js
@@ -1,7 +1,16 @@
 
 
 function randomize () {
-	var color = '#' + (Math.random() * 0xF << 0).toString(16) + (Math.random() * 0xF << 0).toString(16) + (Math.random() * 0xF << 0).toString(16);
+	var rsum = 0;
+	var r = 0;
+	var color = '#';
+	for (var i = 0; i < 3; i++) {
+		r = Math.random();
+		rsum = r + rsum;
+		color += (r * 0xF << 0).toString(16)
+	};
+	if(rsum<1)
+		$(this).css("color", "#FFFFFF");
 	$(this).css("background-color", color);
 	$(this).text(color);
 }
