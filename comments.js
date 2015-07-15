dffa855188e47cef0ac2414ba922cc936dbe01c1
nnah <nahnope@web.de>
2015-07-15 18:16:47 +0200

    comments
    in part 3

---
+++ comments.js
@@ -0,0 +1,27 @@
+var buttonid = 0;
+
+function addComment(i) {
+	buttonid +=1;
+
+	// create a new div and fill it with a new button
+	var div = document.createElement('DIV');
+	div.innerHTML = '<button id="button' + buttonid + '" type="button" onclick="addComment(' + buttonid + ')">Klick</button><br><br>';
+
+	// insert the new div into the clicked button's parent-div
+	var p = document.getElementById("button" + i ).parentElement;
+	p.appendChild(div);
+
+	// depth rises by 1
+	var fb=parseInt(p.getAttribute("fizzbuzz"));
+	fb +=1;
+	div.setAttribute("fizzbuzz", fb);
+
+	// divs in every second depths are gray
+	if (fb % 2 == 0) {
+		div.style.backgroundColor = "gray";
+	}
+	else {
+		div.style.backgroundColor = "white";
+	}
+
+}
